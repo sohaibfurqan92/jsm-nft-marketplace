5fa69309f7d4f2c0ef23dd0266c330da49c62aba
sohaibfurqan92 <sohaibfurqan@hotmail.com>
2023-04-11 22:33:44 +0100

    Setting up routes and custom header/footer components plus font awesome

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { ThemeProvider } from 'next-themes';
+import Script from 'next/script';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import { Navbar, Footer } from '../components';
 
-export default MyApp
+const MyApp = ({ Component, pageProps }) => (
+  <ThemeProvider attribute="class">
+    <div className="dark:bg-nft-dark bg-white min-h-screen">
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+    <Script
+      src="https://kit.fontawesome.com/1f14e011fe.js"
+      crossorigin="anonymous"
+    />
+  </ThemeProvider>
+);
+
+export default MyApp;
